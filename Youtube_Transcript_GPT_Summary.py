0b1fd7eada5e4876561625793f2bf561322ff615
unknown <me@mattguidry.com>
2024-01-09 09:11:32 -0600

    Reorder code with function definitions up top

--- Youtube_Transcript_GPT_Summary.py
+++ Youtube_Transcript_GPT_Summary.py
@@ -3,6 +3,24 @@ import pyperclip
 from youtube_transcript_api import YouTubeTranscriptApi
 import openai
 
+
+openai_pricing = {
+    "gpt-4": {
+        "Input": 0.03 / 1000,
+        "Output": 0.06 / 1000
+    },
+    "gpt-4-32k": {
+        "Input": 0.06 / 1000,
+        "Output": 0.12 / 1000
+    },
+    "gpt-4-1106-preview": {
+        "Input": 0.01 / 1000,
+        "Output": 0.03 / 1000
+    }
+}
+
+model = 'gpt-4-1106-preview'
+
 def get_video_id():
     clipboard_content = pyperclip.paste()
     video_id = extract_video_id(clipboard_content)
@@ -22,21 +40,6 @@ def seconds_to_hms(seconds):
     m, s = divmod(m, 60)
     return f"[{h:02d}:{m:02d}:{s:02d}]"
 
-openai_pricing = {
-    "gpt-4": {
-        "Input": 0.03 / 1000,
-        "Output": 0.06 / 1000
-    },
-    "gpt-4-32k": {
-        "Input": 0.06 / 1000,
-        "Output": 0.12 / 1000
-    },
-    "gpt-4-1106-preview": {
-        "Input": 0.01 / 1000,
-        "Output": 0.03 / 1000
-    }
-}
-
 def calculate_cost(model, input_tokens, output_tokens):
     pricing = openai_pricing.get(model, None)
     if pricing:
@@ -45,7 +48,17 @@ def calculate_cost(model, input_tokens, output_tokens):
         return input_cost + output_cost
     else:
         return 0
+        
+def get_response(messages, model):
+    response = openai.ChatCompletion.create(model=model, messages=messages)
+    prompt_tokens = response['usage']['prompt_tokens']
+    completion_tokens = response['usage']['completion_tokens']
+    return response, prompt_tokens, completion_tokens
 
+
+with open('../OpenAIkey.txt', 'r') as file:
+    openai.api_key = file.readline().strip()
+    
 video_id = get_video_id()
 
 transcript_list = YouTubeTranscriptApi.list_transcripts(video_id)
@@ -54,17 +67,6 @@ transcript_items = transcript.fetch()
 
 compiled_transcript = "\n".join(f"{seconds_to_hms(item['start'])} {item['text']}" for item in transcript_items)
 print(compiled_transcript)
-
-model = 'gpt-4-1106-preview'
-
-with open('OpenAIkey.txt', 'r') as file:
-    openai.api_key = file.readline().strip()
-
-def get_response(messages, model):
-    response = openai.ChatCompletion.create(model=model, messages=messages)
-    prompt_tokens = response['usage']['prompt_tokens']
-    completion_tokens = response['usage']['completion_tokens']
-    return response, prompt_tokens, completion_tokens
 
 messages = [
     {"role": "system", "content": "Provide a brief bullet point summary of the video transcript."},
